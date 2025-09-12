0ce515f0afcea9c586c5be3e84689fd5dc676e0d
eastsierra <150761002+eastsierra@users.noreply.github.com>
2025-09-12 14:00:13 +0800

    :3

---
+++ i3/mouse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''Compx VXE Mouse 1K Dongle' 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop 'pointer:''Compx VXE Mouse 1K Dongle' 'libinput Accel Speed' -0.1
